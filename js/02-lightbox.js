fca218e789cb5f080d31d0ba77ecbe079394e636
Vitalii Mordovets <vitalii.mordovets@gmail.com>
2022-08-24 21:41:04 +0300

    Task-2 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
+
+function createGalleryItemsMarkup(items) {
+    return items.map(({ preview, original, description }) => {
+        return `
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+        `;
+    })
+        .join('');
+} 
+
+new SimpleLightbox('.gallery a', {captionsData: "alt",  captionDelay: 250});
