810a8027d8690238f6f231bb17e27301b303e348
Vitalii Mordovets <vitalii.mordovets@gmail.com>
2022-08-24 21:55:05 +0300

    hw-7 to check

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,14 +1,12 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
 const galleryContainer = document.querySelector('.gallery');
+
 const itemsMarkup = createGalleryItemsMarkup(galleryItems);
 
 galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
 
 galleryContainer.addEventListener('click', onClickModalOpen);
-
-
 
 function createGalleryItemsMarkup(items) {
     return items.map(({ preview, original, description }) => {
